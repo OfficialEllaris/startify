600802afd4d9718c83274104b9f4463f2a36d4b0
Excel Oghenetejiri Prosper <official.ellaris@gmail.com>
2026-05-02 02:44:32 +0100

    feat: add Livewire hook to automatically reload page on 419 session expiration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,27 @@
-//
+const App = {
+    /**
+     * Handles expired page tracking.
+     */
+    initEpiredPageTracker() {
+        Livewire.hook("request", ({ fail }) => {
+            fail(({ status, preventDefault }) => {
+                if (status === 419) {
+                    preventDefault();
+                    location.reload();
+                }
+            });
+        });
+    },
+
+    /**
+     * Bootstraps all features.
+     */
+    init() {
+        document.addEventListener("DOMContentLoaded", () => {
+            this.initEpiredPageTracker();
+        });
+    },
+};
+
+// Start the application
+App.init();
